bec2e4151cbdcc1ea849b2f88453522b1f34bbf3
haykatan <59599762+haykatan@users.noreply.github.com>
2026-01-11 07:51:06 +0200

    add all files

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ansible-playbook create_namespaces.yml
+ansible-playbook install_helm.yml
+ansible-playbook apply_policies.yml
+
+NAMESPACE=elastic
+SVC=kibana-kibana
+SECRET=elasticsearch-master-credentials
+
+KIBANA_IP="$(kubectl -n "$NAMESPACE" get svc "$SVC" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+ELASTIC_USER="$(kubectl -n "$NAMESPACE" get secret "$SECRET" -o jsonpath='{.data.username}' | base64 --decode)"
+ELASTIC_PASS="$(kubectl -n "$NAMESPACE" get secret "$SECRET" -o jsonpath='{.data.password}' | base64 --decode)"
+[ -z "$KIBANA_IP" ] && echo "Kibana LB IP is empty" && exit 1
+
+curl -sS -X POST "http://${KIBANA_IP}:5601/api/saved_objects/_import?overwrite=true" \
+  -u "${ELASTIC_USER}:${ELASTIC_PASS}" \
+  -H "kbn-xsrf: true" \
+  -F file=@kibana-dashboard.ndjson
